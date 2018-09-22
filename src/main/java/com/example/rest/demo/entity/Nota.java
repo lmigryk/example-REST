55357961f2419399c7f8da1b6e54ad35047b4674
Luis migryk <luis.migryk@usach.cl>
2018-09-22 13:13:48 -0300

    alfin

---
+++ src/main/java/com/example/rest/demo/entity/Nota.java
@@ -0,0 +1,63 @@
+package com.example.rest.demo.entity;
+import javax.persistence.*;
+
+@Table(name="NOTA")
+@Entity
+public class Nota {
+
+
+    public Nota(){}
+
+    public Nota(long id,String nombre, String titulo, String contenido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.titulo = titulo;
+        this.contenido = contenido;
+    }
+
+    @GeneratedValue
+    @Id
+    @Column(name="ID_NOTA")
+    private long id;
+    @Column(name="NOMBRE")
+    private String nombre;
+    @Column(name="TITULO")
+    private String titulo;
+    @Column(name="CONTENIDO")
+    private String contenido;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+
+}
